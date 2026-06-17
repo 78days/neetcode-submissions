0452ba924fef9a101114d53a9294fdeaebeb2445
nazgul <alifaisal2005@outlook.com>
2026-06-17 15:49:29 +0500

    Add: max-consecutive-ones-iii - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    longestOnes(nums, k) {
+        let maxLen = 0
+    let l = 0, zeros = 0;
+
+for (let r = 0; r < nums.length; r++) {
+    if (nums[r] === 0) zeros++;
+
+    while (zeros > k) {
+        if (nums[l] === 0) zeros--;
+        l++;
+    }
+
+    maxLen = Math.max(maxLen, r - l + 1);
+}
+return maxLen
+    }
+}
+
+
+
+
+
+
